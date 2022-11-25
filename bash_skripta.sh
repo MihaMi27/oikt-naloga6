a114f714c95ba7e00e5247c7a4d08febac71a08c
Miha Mirt <mihamirt27@gmail.com>
2022-11-25 14:04:24 +0100

    Added overwrite option to docker gpg key

--- bash_skripta.sh
+++ bash_skripta.sh
@@ -44,7 +44,7 @@ apt-get install -y ufw git nginx net-tools
 if apt-get install -y ca-certificates curl gnupg lsb-release
 then
     mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
 apt-get update
